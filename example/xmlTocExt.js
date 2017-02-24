2be5374e8afdbe5aeff2c8bc7f8a76386023b667
Grant Gayed <grant_gayed@ca.ibm.com>
2017-02-24 15:25:57 -0500

    add support for toc file attributes and "xml.toc.file.onGenerate" extension point

--- example/xmlTocExt.js
+++ example/xmlTocExt.js
@@ -3,13 +3,17 @@ var FILENAME_FOOTER = null;
 
 var fs = require("fs");
 var path = require("path");
+var url = require("url");
 var headerText, footerText;
 
 var NOTOC = "notoc";
 var PREFIX_TOC = "toc-";
 
+/* the following regex is sourced from marked: https://github.com/chjj/marked */
+var REGEX_LINK = /^!?\[((?:\[[^\]]*\]|[^\[\]]|\](?=[^\[]*\]))*)\]\(\s*<?([\s\S]*?)>?(?:\s+['"]([\s\S]*?)['"])?\s*\)/;
+
 var html = {};
-var xml = {toc: {}};
+var xml = {toc: {file: {}}};
 
 html.onHeading = function(html, data) {
 	var heading = data.htmlToDom(html);
@@ -24,7 +28,7 @@ html.onHeading = function(html, data) {
 		return; /* nothing to do */
 	}
 	return data.domToHtml(heading);
-}
+};
 
 xml.toc.onTopic = function(topic, data) {
 	var heading = data.htmlToDom(data.heading);
@@ -49,7 +53,7 @@ xml.toc.onTopic = function(topic, data) {
 	}
 
 	return data.domToHtml(topicDom, {xmlMode: true});
-}
+};
 
 xml.toc.onComplete = function(xml, data) {
 	if (!headerText && !footerText) {
@@ -66,7 +70,70 @@ xml.toc.onComplete = function(xml, data) {
 	}
 
 	return result;
-}
+};
+
+xml.toc.file.onGenerate = function(xml, data) {
+	var CLASS_TOC = "toc";
+	var CLASS_NAVGROUP = "navgroup";
+	var CLASS_TOPICGROUP = "topicgroup";
+	var ATTRIBUTE_CLASS = "class";
+
+	/* if injecting TOC from a .md file add an <anchor> child to all non-top-level topics */
+	if (/\.md\s*$/.test(data.source)) {
+		var root = data.htmlToDom(xml);
+		var topics = data.domUtils.find(function(node) {return node.name && node.name.toLowerCase() === "topic";}, [root], true, Infinity);
+		topics.forEach(function(topic) {
+			if (topic.parent) {
+				var urlObject = url.parse(topic.attribs.href);
+				var pathname = urlObject.pathname.substring(urlObject.pathname.lastIndexOf("/") + 1);
+				pathname = pathname.substring(0, pathname.lastIndexOf("."));
+				var anchorId = pathname + "_" + urlObject.hash.substring(1);
+				var anchor = data.htmlToDom('<anchor id="' + anchorId + '" />');
+				var children = data.domUtils.getChildren(topic);
+				if (!children.length) {
+					data.domUtils.appendChild(topic, anchor);
+				} else {
+					var firstChild = children[0];
+					data.domUtils.prepend(firstChild, anchor);
+				}
+			}
+		});
+		return data.domToHtml(root, {xmlMode: true});
+	}
+
+	var match = REGEX_LINK.exec(data.source);
+	if (match) {
+		/* link to external content */
+		return '<topic href="' + match[2] + '" label="' + match[1] + '" />';
+	}
+
+	/* check for custom elements this extension knows how to generate */
+	var classes = data.attributes[ATTRIBUTE_CLASS];
+	if (classes) {
+		classes = classes.split(" ");
+		for (var i = 0; i < classes.length; i++) {
+			var current = classes[i].toLowerCase();
+			if (current === CLASS_TOC) {
+				var propertiesString = "";
+				var keys = Object.keys(data.attributes);
+				keys.forEach(function(key) {
+					if (key.toLowerCase() !== ATTRIBUTE_CLASS) {
+						propertiesString += '<property name="' + key + '" value="' + data.attributes[key] + '" />\n';
+					}			
+				});
+				return '<toc label="' + data.source + '">\n' + propertiesString + '</toc>';
+			}
+			if (current === CLASS_NAVGROUP) {
+				return '<topic label="' + data.source + '"><property name="navgroup" value="' + data.source + '" /></topic>';
+			}
+			if (current === CLASS_TOPICGROUP) {
+				return '<topic label="' + data.source + '"><property name="topicgroup" value="' + data.source + '" /></topic>';
+			}
+		}
+	}
+
+	return null;
+};
 
 var init = function(data) {
 	if (FILENAME_HEADER) {
