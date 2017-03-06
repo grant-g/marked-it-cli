6453c13d4235f6f8d44b051e9ddde39ff9edb563
Grant Gayed <grant_gayed@ca.ibm.com>
2017-03-06 12:40:35 -0500

    adapt xmlTocExt.js navgroups to flat

--- example/xmlTocExt.js
+++ example/xmlTocExt.js
@@ -4,7 +4,9 @@ var FILENAME_FOOTER = null;
 var fs = require("fs");
 var path = require("path");
 var url = require("url");
+
 var headerText, footerText;
+var logger;
 
 var NOTOC = "notoc";
 var PREFIX_TOC = "toc-";
@@ -72,72 +74,137 @@ xml.toc.onComplete = function(xml, data) {
 	return result;
 };
 
+var navgroup;
+
 xml.toc.file.onGenerate = function(xml, data) {
 	var CLASS_TOC = "toc";
 	var CLASS_NAVGROUP = "navgroup";
+	var CLASS_NAVGROUP_END = "navgroup-end";
 	var CLASS_TOPICGROUP = "topicgroup";
 	var ATTRIBUTE_CLASS = "class";
+	var ATTRIBUTE_ID = "id";
+
+	var clearNavgroupAtEnd = false;
+
+	/* check for .navgroup attribute */
+	var classes = data.attributes[ATTRIBUTE_CLASS];
+	if (classes) {
+		classes = classes.split(" ");
+		for (var i = 0; i < classes.length; i++) {
+			var className = classes[i].toLowerCase();
+			if (className === CLASS_NAVGROUP) {
+				if (navgroup) {
+					logger.warn("Encountered new .navgroup before previous .navgroup '" + navgroup.id + "' was ended");
+				} else {
+					var id = data.attributes[ATTRIBUTE_ID];
+					if (!id) {
+						logger.warn("Encountered .navgroup without an 'id' attribute, on toc item: " + data.source);
+					} else {
+						navgroup = {id: id, level: data.level};
+					}					
+				}
+			} else if (className === CLASS_NAVGROUP_END) {
+				if (!navgroup) {
+					logger.warn("Encountered .navgroup-end while not in a previous .navgroup, on toc item: " + data.source);
+				}
+				clearNavgroupAtEnd = true;
+			}
+		}
+	}
+
+	if (navgroup && data.level < navgroup.level) {
+		logger.warn("Missing .navgroup-end for .navgroup '" + navgroup.id + "' (must be present and at the same indentation level)");
+		navgroup = null;
+	}
+
+	var element;
 
 	/* if injecting TOC from a .md file then replace all immediate children with <anchor>s */
 	if (/\.md\s*$/.test(data.source)) {
-		var root = data.htmlToDom("<root></root>", {xmlMode: true})[0];
+		element = data.htmlToDom("<root></root>", {xmlMode: true})[0];
 		var docRoots = data.htmlToDom(xml, {xmlMode: true});
 		docRoots.forEach(function(docRoot) {
-			data.domUtils.appendChild(root, docRoot);
+			data.domUtils.appendChild(element, docRoot);
 			if (docRoot.name === "topic") {
 				var children = data.domUtils.getChildren(docRoot);
 				children.forEach(function(child) {
 					if (child.attribs) {
 						var anchorElement = data.htmlToDom('<property name="type" value="anchor" />', {xmlMode: true})[0];
-						data.domUtils.appendChild(child, anchorElement);
+						var childChildren = data.domUtils.getChildren(child);
+						if (childChildren.length) {
+							data.domUtils.prepend(childChildren[0], anchorElement);
+						} else {
+							data.domUtils.appendChild(child, anchorElement);
+						}
 					}
 				});
+				if (navgroup && data.level === navgroup.level) {
+					var navgroupElement = data.htmlToDom('<property name="navgroup" value="' + navgroup.id + '" />', {xmlMode: true})[0];
+					var elementChildren = data.domUtils.getChildren(docRoot);
+					if (elementChildren.length) {
+						data.domUtils.prepend(elementChildren[0], navgroupElement);
+					} else {
+						data.domUtils.appendChild(docRoot, navgroupElement);
+					}
+				}
 			}
 		});
-		return data.domToInnerHtml(root, {xmlMode: true});
+		navgroup = clearNavgroupAtEnd ? null : navgroup;
+		return data.domToInnerHtml(element, {xmlMode: true});
 	}
 
 	var match = REGEX_LINK.exec(data.source);
 	if (match) {
 		/* link to external content */
-		return '<topic href="' + match[2] + '" label="' + match[1] + '" />';
-	}
-
-	/* check for custom elements this extension knows how to generate */
-	var classes = data.attributes[ATTRIBUTE_CLASS];
-	if (classes) {
-		classes = classes.split(" ");
-		for (var i = 0; i < classes.length; i++) {
-			var current = classes[i].toLowerCase();
-			if (current === CLASS_TOC) {
-				var propertiesString = "";
-				var keys = Object.keys(data.attributes);
-				keys.forEach(function(key) {
-					if (key.toLowerCase() !== ATTRIBUTE_CLASS) {
-						propertiesString += '<property name="' + key + '" value="' + data.attributes[key] + '" />\n';
-					}			
-				});
-				return '<toc label="' + data.source + '">\n' + propertiesString + '</toc>';
-			}
-			if (current === CLASS_NAVGROUP) {
-				return '<topic label="' + data.source + '"><property name="navgroup" value="' + data.source + '" /></topic>';
-			}
-			if (current === CLASS_TOPICGROUP) {
-				return '<topic label="' + data.source + '"><property name="topicgroup" value="' + data.source + '" /></topic>';
+		element = data.htmlToDom('<topic href="' + match[2] + '" label="' + match[1] + '" />', {xmlMode: true})[0];
+	} else {
+		/* check for custom elements this extension knows how to generate */
+		var classes = data.attributes[ATTRIBUTE_CLASS];
+		if (classes) {
+			classes = classes.split(" ");
+			for (var i = 0; i < classes.length; i++) {
+				var current = classes[i].toLowerCase();
+				if (current === CLASS_TOC) {
+					var propertiesString = "";
+					var keys = Object.keys(data.attributes);
+					keys.forEach(function(key) {
+						if (key.toLowerCase() !== ATTRIBUTE_CLASS) {
+							propertiesString += '<property name="' + key + '" value="' + data.attributes[key] + '" />\n';
+						}			
+					});
+					element = data.htmlToDom('<toc label="' + data.source + '">\n' + propertiesString + '</toc>', {xmlMode: true})[0];
+					break;
+				}
+				if (current === CLASS_TOPICGROUP) {
+					element = data.htmlToDom('<topic label="' + data.source + '"><property name="topicgroup" value="' + data.source + '" /></topic>', {xmlMode: true})[0];
+					break;
+				}
 			}
 		}
 	}
 
-	return null;
+	if (element && navgroup && data.level === navgroup.level) {
+		var navgroupElement = data.htmlToDom('<property name="navgroup" value="' + navgroup.id + '" />', {xmlMode: true})[0];
+		var elementChildren = data.domUtils.getChildren(element);
+		if (elementChildren.length) {
+			data.domUtils.prepend(elementChildren[0], navgroupElement);
+		} else {
+			data.domUtils.appendChild(element, navgroupElement);
+		}
+	}
+
+	navgroup = clearNavgroupAtEnd ? null : navgroup;
+	return element ? data.domToHtml(element, {xmlMode: true}) : null;
 };
 
 var init = function(data) {
+	logger = data.logger;
 	if (FILENAME_HEADER) {
 		var headerPath = path.join(__dirname, FILENAME_HEADER);
 		try {
 			var fd = fs.openSync(headerPath, "r");
 		} catch (e) {
-			data.logger.error("Failed to open XML header file: " + headerPath + "\n" + e.toString());
+			logger.error("Failed to open XML header file: " + headerPath + "\n" + e.toString());
 		}
 		if (fd) {
 			headerText = data.readFile(fd);
@@ -150,7 +217,7 @@ var init = function(data) {
 		try {
 			var fd = fs.openSync(footerPath, "r");
 		} catch (e) {
-			data.logger.error("Failed to open XML footer file: " + footerPath + "\n" + e.toString());
+			logger.error("Failed to open XML footer file: " + footerPath + "\n" + e.toString());
 		}
 		if (fd) {
 			footerText = data.readFile(fd);
